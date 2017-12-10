567c2cb18e1dce14440a7794fd35bae835a405b7
Sung <sungp2k@gmail.com>
2017-12-10 09:26:36 -0800

    sidebar render works 2

--- js/retain.js
+++ js/retain.js
@@ -20,13 +20,14 @@ $(function(){
     var octopus = {
         addNewNote: function(noteStr) {
             model.add({
-                content: noteStr
+                content: noteStr,
+                date: Date.now()
             });
             view.render();
         },
 
         getNotes: function() {
-            return model.getAllNotes();
+            return model.getAllNotes().reverse();
         },
 
         init: function() {
@@ -52,8 +53,13 @@ $(function(){
             var htmlStr = '';
             octopus.getNotes().forEach(function(note){
                 htmlStr += '<li class="note">'+
-                        note.content +
+                        '<span class="note-date">' + 
+                    (new Date(note.date)).toString("en-US") +
+                    '</span>' +
+                    note.content +
                     '</li>';
+                        
+                
             });
             this.noteList.html( htmlStr );
         }
